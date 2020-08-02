8e53f312cc0de1a2d51df836c5e9dff77a465dd3
neil <naha.hrithkk@gmail.com>
2020-08-02 15:20:16 +0530

    Image add functionality added

--- src/main/java/com/naha/crimereportingsystem/CRSApplication.java
+++ src/main/java/com/naha/crimereportingsystem/CRSApplication.java
@@ -1,13 +1,18 @@
 package com.naha.crimereportingsystem;
 
+import java.io.File;
+
+import com.naha.crimereportingsystem.images.ImageController;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class CRSApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		new File(ImageController.uploadDirectory).mkdir();
+		SpringApplication.run(CRSApplication.class, args);
 	}
 
 }
---
+++ src/main/webapp/WEB-INF/jsp/image/view.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>CRS</title>
+	</head>
+	<body>
+		<h1>Check</h1>
+		<img src="/images/Screenshot (9).png" alt="" />
+	</body>
+</html>
